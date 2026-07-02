2ebac1ef9c65ace92ae738116368985792c9acee
03Filip0001 <hackboyiv@gmail.com>
2026-07-02 18:47:11 +0200

    AddWindow setup

---
+++ ScadaGUI/AddWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ScadaGUI
+{
+    /// <summary>
+    /// Interaction logic for AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        public AddWindow()
+        {
+            InitializeComponent();
+
+            Dropdown.Items.Add("AI");
+            Dropdown.Items.Add("AO");
+
+            Dropdown.Items.Add("DI");
+            Dropdown.Items.Add("DO");
+
+            Dropdown.Items.Add("Alarm");
+
+        }
+    }
+}
